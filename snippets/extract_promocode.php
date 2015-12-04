79aeca9feec533ed5dfa40f6407d507e485f3254
antoncom <profit3845>
2015-12-04 16:18:08 +0300

    show promode for blogger and set start_date, end_date and blogger_id

---
+++ snippets/extract_promocode.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Извлечь промокод
+ * на входе pa_id
+ * на выходе свободнф промокод
+ */
+
+if(!empty($scriptProperties['pa_id']))	{
+	$pa_code = $scriptProperties['pa_id'];
+
+	// получаем ближайший свободный промо-код
+	$q = $modx->newQuery('modResource');
+	$q->select('modResource.id AS id,pagetitle');
+	$q->leftJoin('modTemplateVarResource', 'modTemplateVarResourceBlg', array('modTemplateVarResourceBlg.tmplvarid = 5',
+		'modTemplateVarResourceBlg.contentid = modResource.id'));
+	$q->leftJoin('modTemplateVarResource', 'modTemplateVarResourcePa', array('modTemplateVarResourcePa.tmplvarid = 15',
+		'modTemplateVarResourcePa.contentid = modResource.id'));
+
+	$q->where(array('parent' => 5135,
+		'modTemplateVarResourceBlg.value:IS' => NULL));
+	$q->sortby('pagetitle', 'ASC');
+	$q->limit(1);
+	$q->prepare();
+
+	$q->stmt->execute();
+	$res = $q->stmt->fetchAll(PDO::FETCH_ASSOC);
+
+	// Присваиваем блоггера для данного промокода
+	$user = $modx->getUser();
+	$userId = $user->get('id');
+	$pcode = $modx->getObject('modResource', $res[0]['id']);
+	if (!$pcode->setTVValue('blogger_id', $userId)) {
+		$modx->log(xPDO::LOG_LEVEL_ERROR, 'EXTRACT_PROMOCODE SNIPPET: There was a problem saving your TV blogger_id');
+	}
+
+	// Присваиваем начальную и конечную дату промо-кода
+	$now = date("Y-m-d H:i:s", time());
+	if (!$pcode->setTVValue('pc_start_date', $now)) {
+		$modx->log(xPDO::LOG_LEVEL_ERROR, 'EXTRACT_PROMOCODE SNIPPET: There was a problem saving your TV pc_start_date');
+	}
+	$in2months = date('Y-m-d H:i:s', strtotime($now .' +2 month'));
+	if (!$pcode->setTVValue('pc_end_date', $in2months)) {
+		$modx->log(xPDO::LOG_LEVEL_ERROR, 'EXTRACT_PROMOCODE SNIPPET: There was a problem saving your TV pc_start_date');
+	}
+
+	return $res[0]['pagetitle'];
+}
