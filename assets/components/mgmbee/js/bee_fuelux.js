b6886590e13d5a6727fbf464d4e39d99e1fa0092
mrfine <mrfine@goodmaster.info>
2015-12-10 21:21:16 +0300

    fuelux initial adding

---
+++ assets/components/mgmbee/js/bee_fuelux.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+//	$('#profile_wizard').wizard('selectedItem', { step: 2 });
+	$('#profile_wizard').wizard();
+	$('#profile_wizard').find('ul.steps li').toggleClass('complete', true);
+
+	$('#profile_wizard').on('changed.fu.wizard', function (evt, data) {
+		$('#profile_wizard').find('ul.steps li').toggleClass('complete', true);
+		console.log('here');
+	});
+
+});
