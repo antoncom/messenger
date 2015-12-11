03ba9bef878c4f4deda0209896c30a8be29febf5
mrfine <mrfine@goodmaster.info>
2015-12-11 22:26:29 +0300

    date range picker added to profile

---
+++ assets/components/mgmbee/js/bee_datepicker.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+	$('input[name="bee_ajax_dob"]').daterangepicker({
+		"singleDatePicker": true,
+		"showDropdowns": true,
+		"autoApply": true,
+		"locale": {
+			"format": "DD/MM/YYYY",
+			"separator": "/",
+			"applyLabel": "Применить",
+			"cancelLabel": "Сброс",
+			"fromLabel": "От",
+			"toLabel": "До",
+			"customRangeLabel": "Произвольно",
+			"daysOfWeek": [
+				"Вс",
+				"Пн",
+				"Вт",
+				"Ср",
+				"Чт",
+				"Пт",
+				"Сб"
+			],
+			"monthNames": [
+				"Январь",
+				"Февраль",
+				"Март",
+				"Апрель",
+				"Май",
+				"Июнь",
+				"Июль",
+				"Август",
+				"Сентябрь",
+				"Октярбрь",
+				"Ноябрь",
+				"Декабрь"
+			],
+			"firstDay": 1
+		}
+	},
+	function(start, end, label) {
+		//
+	});
+});
