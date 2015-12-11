8d73ed7ab042054a94531a79877bb3b07cfac304
antoncom <profit3845>
2015-12-11 16:14:56 +0300

    user profile added (step 1), first release

--- assets/components/mgmbee/js/bee_fuelux.js
+++ assets/components/mgmbee/js/bee_fuelux.js
@@ -7,5 +7,16 @@ $(document).ready(function() {
 		$('#profile_wizard').find('ul.steps li').toggleClass('complete', true);
 		console.log('here');
 	});
+	//d = $('#myDatepickerInput').val();
+
+	d = '1449834498';
+	console.log(d);
+	console.log(parseInt(d));
+	dd = new Date(parseInt(d));
+	console.log(dd);
+
+	aa = new Date('December 17, 1995 03:24:00');
+
+	$('#myDatepicker').datepicker('setDate', aa);
 
 });
---
+++ snippets/prf_field.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Get regular field in profile
+ * $field_name - name attribute for input field. It has to be equal modUser profile field
+ * $provider - snippet name which provide data for the field
+ * $field_header - for fuelux glass
+ * $field_footer - for fuelux glass
+ */
+
+if(!empty($field_name))	{
+	$modx->getService('lexicon','modLexicon');
+	$modx->setOption('cultureKey', 'ru');
+	$modx->lexicon->load('beecore:default');
+
+	$prefix = 'bee_ajax_'; // prefix for input field name, which is used in 'bee_ajax' snippet
+	$user = $modx->user;
+	$profile = $user->getOne('Profile');
+	if ($profile) {
+		$extended = $profile->get('extended');
+	}
+
+	// run appropriative snippet to get data
+	switch($provider)	{
+		case('get_blogger_phone'):
+
+			break;
+		case('get_blogger_card'):
+
+			break;
+
+		default:
+			// if provider is not defined then get regular modUser field
+			switch($field_name)	{
+				case('photo'):
+					$out = $modx->getChunk('profile_photo', array('field_value' => $profile->get($field_name)));
+					break;
+				case('mobilephone'):
+					$phone = $modx->runSnippet('get_blogger_phone');
+					$out = $modx->getChunk('profile_fuelux_header', array(
+							'field_name' => $prefix . $field_name,
+							'field_label' => $modx->lexicon('beecore.' . $field_name),
+							'field_footer' => 'Прислать SMS с кодом',
+							'field_value' => $phone));
+					break;
+
+				case('dob'):
+					$out = $modx->getChunk('profile_birthday', array(
+							'field_name' => $prefix . $field_name,
+							'field_label' => $modx->lexicon('beecore.' . $field_name),
+							'field_value' => $profile->get($field_name)));
+					break;
+
+				default:
+					// if provider is not set
+					// then get data from modUser normal field or profile
+					$from_profile = (in_array($field_name, array('username', 'password')) == FALSE);
+					$out = $modx->getChunk('profile_text_input', array(
+							'field_name' => $prefix . $field_name,
+							'field_label' => $modx->lexicon('beecore.' . $field_name),
+							'field_value' => ($from_profile) ? $profile->get($field_name) : $user->get($field_name)));
+			}
+
+	}
+	return $out;
+}
